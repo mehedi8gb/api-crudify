9be395a14a1d1240be5f398ed4f3af8507f412e2
MD Mehedi Hasan <mehidy.gb@gmail.com>
2023-09-09 01:54:24 +0600

    form request structure improved!

--- src/Stubs/CreateFormRequest.php
+++ src/Stubs/CreateFormRequest.php
@@ -27,52 +27,53 @@ class {$this->modelBinding['className']}StoreRequest extends FormRequest
         return true; // Authorization logic goes here (e.g., check if the user has permission)
     }
 
-protected function prepareForValidation(): void
+    protected function prepareForValidation(): void
+        {
+            // You can perform data manipulation here
+            \$this->merge([
+                'title' => ucwords(strtolower(\$this->input('title'))), // Convert name to title case
+                // Add more data manipulation as needed
+            ]);
+        }
+
+    public function rules(): array
     {
-        // You can perform data manipulation here
-        \$this->merge([
-            'title' => ucwords(strtolower(\$this->input('title'))), // Convert name to title case
-            // Add more data manipulation as needed
-        ]);
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:1000',
+            'slug' => 'nullable',
+            // 'image' => 'required|image|mimes:jpeg,png|max:2048', // Example validation rules
+        ];
     }
 
-public function rules(): array
-{
-    return [
-        'title' => 'required|string|max:255',
-        'description' => 'required|string|max:1000',
-        'slug' => 'nullable',
-        // 'image' => 'required|image|mimes:jpeg,png|max:2048', // Example validation rules
-    ];
-}
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'The title field is required.',
+            'description.required' => 'The description field is required.',
+            // 'image.uploaded' => 'The image must be a valid JPEG or PNG file.',
+            // Define custom validation error messages here
+        ];
+    }
 
-public function messages(): array
-{
-    return [
-        'title.required' => 'The title field is required.',
-        'description.required' => 'The description field is required.',
-        // 'image.uploaded' => 'The image must be a valid JPEG or PNG file.',
-        // Define custom validation error messages here
-    ];
-}
+    public function attributes(): array
+    {
+        return [
+            //'title' => 'product title',
+            //'description' => 'product description',
+            // 'image' => 'product image',
+            // Define custom attribute names for validation error messages here
+        ];
+    }
 
-public function attributes(): array
-{
-    return [
-        //'title' => 'product title',
-        //'description' => 'product description',
-        // 'image' => 'product image',
-        // Define custom attribute names for validation error messages here
-    ];
-}
-
-protected function passedValidation(): void
-{
-    // You can further manipulate the validated data after it has been inserted into the database
-    // For example, you can hash the password before storing it in the database
-    \$this->replace([
-        //
-    ]);
+    protected function passedValidation(): void
+    {
+        // You can further manipulate the validated data after it has been inserted into the database
+        // For example, you can hash the password before storing it in the database
+        \$this->replace([
+            //
+        ]);
+    }
 }
         ";
     }
@@ -87,55 +88,56 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class {$this->modelBinding['className']}UpdateRequest extends FormRequest
 {
-public function authorize()
-{
-    return true; // Authorization logic goes here (e.g., check if the user has permission)
-}
+    public function authorize()
+    {
+        return true; // Authorization logic goes here (e.g., check if the user has permission)
+    }
 
-protected function prepareForValidation(): void
-{
-    // You can perform data manipulation here
-    \$this->merge([
-        'title' => ucwords(strtolower(\$this->input('title'))), // Convert name to title case
-        // Add more data manipulation as needed
-    ]);
-}
+    protected function prepareForValidation(): void
+    {
+        // You can perform data manipulation here
+        \$this->merge([
+            'title' => ucwords(strtolower(\$this->input('title'))), // Convert name to title case
+            // Add more data manipulation as needed
+        ]);
+    }
 
-public function rules(): array
-{
-    return [
-        'title' => 'nullable|string|max:255',
-        'description' => 'nullable|string|max:1000',
-        'slug' => 'nullable',
-        // 'image' => 'required|image|mimes:jpeg,png|max:2048', // Example validation rules
-    ];
-}
+    public function rules(): array
+    {
+        return [
+            'title' => 'nullable|string|max:255',
+            'description' => 'nullable|string|max:1000',
+            'slug' => 'nullable',
+            // 'image' => 'required|image|mimes:jpeg,png|max:2048', // Example validation rules
+        ];
+    }
 
-public function messages(): array
-{
-    return [
-        // 'image.uploaded' => 'The image must be a valid JPEG or PNG file.',
-        // Define custom validation error messages here
-    ];
-}
+    public function messages(): array
+    {
+        return [
+            // 'image.uploaded' => 'The image must be a valid JPEG or PNG file.',
+            // Define custom validation error messages here
+        ];
+    }
 
-public function attributes(): array
-{
-    return [
-        'title' => 'product title',
-        'description' => 'product description',
-        // 'image' => 'product image',
-        // Define custom attribute names for validation error messages here
-    ];
-}
+    public function attributes(): array
+    {
+        return [
+            'title' => 'product title',
+            'description' => 'product description',
+            // 'image' => 'product image',
+            // Define custom attribute names for validation error messages here
+        ];
+    }
 
-protected function passedValidation(): void
-{
-    // You can further manipulate the validated data after it has been inserted into the database
-    // For example, you can hash the password before storing it in the database
-    \$this->replace([
-        //
-    ]);
+    protected function passedValidation(): void
+    {
+        // You can further manipulate the validated data after it has been inserted into the database
+        // For example, you can hash the password before storing it in the database
+        \$this->replace([
+            //
+        ]);
+    }
 }
         ";
     }
